7e0409c17e473d713c3ed261f7b9170b1027fbe4
kavyashine <68185911+kavyashine@users.noreply.github.com>
2020-11-12 14:18:31 +0530

    first react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Card from "./Cards";
+import "./index.css";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<>
+<h1 className="heading">List of Netflix Series</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<Card 
+imgsrc ="https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZnqiiq3IsmJ8FqPUYey8DqELi_7Zo43KuseSDuReHbCK5dtM0Kkf9XGotSjJCgSRK9Jjegp-Gq-TM9jMbtnlGPFTLAN.jpg?r=7ad"
+title =" A Netflix Original Series" 
+sname="DRISHYAM"
+link ="https://www.netflix.com/in/title/80068117"/>
+
+<Card
+imgsrc ="https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYdV09MEfh-o-ZaQKy-Ce9zyi10-LtdC33BS3PiSG6xXL_lugGa8dn4h8reEQF--edVFapIUfZlE6u-_KI_KA_EWxCPt.jpg?r=81c"
+title =" A Netflix Original Series" 
+sname="SAAHO"
+link ="https://www.netflix.com/in/title/81157385"/>
+
+<Card
+imgsrc ="https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUYq7X_H9BSp5RslX4VDN0BOdKpbVLeJXBtMWbY0oUidKlZ4vOZE4HO_di4XF9zWC2og3iBXECWy1-r3jRswA8UlSJtB.jpg?r=b07"
+title =" A Netflix Original Series" 
+sname="PHANTOM"
+link ="https://www.netflix.com/in/title/70303495"
+/>
+<Card
+  imgsrc ="https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcaAdh1hiQu4lPVMBh9JrgB6uSZsdrSXmhILUCpchyf0JiuJnLhZTfSc4ZlHpf87jPXcRUUjEv37focM6mouKaYmDKd4.jpg?r=133"
+title =" A Netflix Original Series" 
+sname="NAAM SHABANA"
+link ="https://www.netflix.com/in/title/80177776"
+/>
+<Card
+  imgsrc ="https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABadFnKB61tZYh6xT_SXzcfmzGvCgbXG5a0OkPJWXa0yW1zL_OZEqgQ6lwP_0-FTRwxnEeL3_5NpT22fa35q3Aoswd4pX.jpg?r=e72"
+title =" A Netflix Original Series" 
+sname="HAPPY NEW YEAR"
+link ="https://www.netflix.com/in/title/80017528"
+/>
+<Card
+  imgsrc ="https://occ-0-2611-3662.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABYsyTmHXu3bAcK77KPoT1wOx4o3JiZkWJv_6z-FP5flkff6xcOqrDn4aqZGwPVIOW90wHxapanglQDhdFbIkE2fRbmNQ.jpg?r=a5e"
+title =" A Netflix Original Series" 
+sname="JUDWAA 2"
+link ="https://www.netflix.com/in/title/80236906"
+/>
+
+</>,
+document.getElementById('root')
+);
